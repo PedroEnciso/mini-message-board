4b2abc750590aed7eba672a690ba593f2c431e50
Pedro Enciso <penciso@ucsd.edu>
2021-07-17 09:40:44 -0700

    move new.js into index.js

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,9 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+/* GET add message page. */
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Add a message" });
+});
+
 module.exports = router;
