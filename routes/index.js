e74b92385f3488719cc772b7ddc895d4c12ca92c
Pedro Enciso <penciso@ucsd.edu>
2021-07-17 08:15:53 -0700

    data, scripts and gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there! Welcome to my message board.",
+    user: "Pedro",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Random User",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
